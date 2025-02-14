cb819c40b72e6e55cb08adfe4ce3c4a2aa8bfd8d
Petrik Diak <diak@petrik.hu>
2025-02-14 11:30:11 +0100

    File beolvasas kesz

--- fileorai/Program.cs
+++ fileorai/Program.cs
@@ -2,9 +2,39 @@
 {
 	internal class Program
 	{
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+					 List<Karakter> karakterek = [];
+
+			fileBeolvas("karakterek.txt",karakterek);
+			foreach(var item in karakterek)
+			{
+				Console.WriteLine(item);
+
+			}
+			
+			
 		}
+
+		static void fileBeolvas(string file, List<Karakter> karakterek)
+		{
+			StreamReader sr = new(file);
+			sr.ReadLine();
+
+			while (!sr.EndOfStream)
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(";");
+
+				Karakter karakter = new(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
+
+
+		}
+
+		
+		
 	}
 }
